8eb1aea4ac0320a4a9c4247ee8bafb6a7ffcb667
Md. Salahuddin <salah_cse_mbstu@yahoo.com>
2019-10-07 19:15:34 +0600

    update fillable

--- app/Book.php
+++ app/Book.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    protected $fillable = ['name'];
+    protected $fillable = ['name', 'isbn_no', 'price', 'description'];
 
     public function user()
     {
